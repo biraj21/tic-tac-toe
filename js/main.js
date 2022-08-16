44ef869b1f3e19f65d30a7736c9c606561cac445
Biraj <biraj.pub@gmail.com>
2022-08-16 17:54:38 +0530

    settings removed & code refactored

--- js/main.js
+++ js/main.js
@@ -11,9 +11,12 @@ const WIN_COMBOS = [
     [2, 4, 6],
 ];
 
-const DIFFICULTY_EASY = "easy";
-const DIFFICULTY_MEDIUM = "medium";
-const DIFFICULTY_IMPOSSIBLE = "impossible";
+const EASY = "Easy";
+const MEDIUM = "Medium";
+const IMPOSSIBLE = "Impossible";
+
+const SINGLE_PLAYER = "Computer";
+const MULTI_PLAYER = "2 Players";
 
 const O = "o";
 const X = "x";
@@ -21,360 +24,335 @@ const X = "x";
 const DRAW = "draw";
 
 const game = {
-    dark_theme: true,
+    darkTheme: true,
     mode: null,
     difficulty: null,
-    sound: true,
-    tap_sound: null,
-
-    play_sound() {
-        if (game.sound) {
-            game.tap_sound.currentTime = 0;
-            game.tap_sound.play();
-        }
-    }
 };
 
 const player1 = {
-        name: "Player 1",
-        symbol: X,
-        score: 0,
-    },
-    player2 = {
-        name: "Player 2",
-        symbol: O,
-        score: 0,
-    };
-
-let current_player = player1;
-const board_state = Array(9);
-
-// elements
-let home_el, difficulty_el, game_el, board_el, cells_el, p1_name_el, p2_name_el;
-
-window.onload = e => {
-    home_el = $("#home");
-    difficulty_el = $("#difficulty");
-    game_el = $("#game");
-    board_el = $("#board");
-    cells_el = document.querySelectorAll(".cell");
-    p1_name_el = $("#p1-name");
-    p2_name_el = $("#p2-name");
-
-    game.tap_sound = new Audio("/audio/tapsound.mp3");
-    cells_el.forEach((cell, i) => cell.setAttribute("data-index", i));
-
-    // adding events
-    document.querySelectorAll("#symbols .symbol")
-        .forEach(symbol => symbol.onclick = choose_symbol);
-
-    $("#home-btn").onclick = () => {
-        game_el.className = "inactive";
-        difficulty_el.className = "inactive";
-        home_el.className = "active";
-    };
-
-    const settings_el = $("#settings");
-    $("#settings-btn").onclick = () => settings_el.classList.add("active");
-    $("#save-settings-btn").onclick = () => settings_el.classList.remove("active");
-
-    document.querySelectorAll(".toggle-btn")
-        .forEach(btn => btn.addEventListener(
-            "click",
-            () => btn.classList.toggle("toggle-btn--on")
-        ));
-
-    $("#theme-btn").onclick = change_theme;
-    $("#sound-btn").onclick = () => game.sound = !game.sound;
-
-    $("#single-player-btn").onclick = () => {
-        game.mode = "single-player";
-
-        player1.name = "You";
-        player2.name = "Computer";
-
-        home_el.className = "inactive";
-        difficulty_el.className = "active";
-
-        game.active_page = difficulty_el;
-    };
-
-    $("#multi-player-btn").onclick = () => {
-        game.mode = "multi-player";
-
-        player1.name = "Player 1";
-        player2.name = "Player 2";
-
-        start_game();
-    };
-
-    $("#easy-btn").onclick = () => {
-        game.difficulty = DIFFICULTY_EASY;
-        start_game();
-    };
-
-    $("#medium-btn").onclick = () => {
-        game.difficulty = DIFFICULTY_MEDIUM;
-        start_game();
-    };
-
-    $("#impossible-btn").onclick = () => {
-        game.difficulty = DIFFICULTY_IMPOSSIBLE;
-        start_game();
-    };
-
-    $("#restart-btn").onclick = () => start_game(false);
+    name: "Player 1",
+    symbol: X,
+    score: 0,
 };
 
-function $(selector) {
+const player2 = {
+    name: "Player 2",
+    symbol: O,
+    score: 0,
+};
+
+let currentPlayer = player1;
+const boardState = Array(9);
+
+// selecting elements
+const $home = get("#home");
+const $difficulty = get("#difficulty");
+const $game = get("#game");
+const $gameMode = get("#game-mode");
+const $board = get("#board");
+const $cells = getAll(".cell");
+const $p1Name = get("#p1-name");
+const $p2Name = get("#p2-name");
+const $sunSvg = get("#sun-svg");
+const $moonSvg = get("#moon-svg");
+
+$cells.forEach(($cell, i) => ($cell.dataset.index = i));
+
+// adding events
+getAll("#symbols .symbol").forEach(($symbol) => {
+    $symbol.addEventListener("click", chooseSymbol);
+});
+
+get("#home-btn").addEventListener("click", () => {
+    $game.className = "inactive";
+    $difficulty.className = "inactive";
+    $home.className = "active";
+});
+
+get("#theme-btn").addEventListener("click", changeTheme);
+
+get("#single-player-btn").addEventListener("click", () => {
+    game.mode = SINGLE_PLAYER;
+
+    player1.name = "You";
+    player2.name = "Computer";
+
+    $home.className = "inactive";
+    $difficulty.className = "active";
+});
+
+get("#multi-player-btn").addEventListener("click", () => {
+    game.mode = MULTI_PLAYER;
+
+    player1.name = "Player 1";
+    player2.name = "Player 2";
+
+    startGame();
+});
+
+for (const $btn of getAll("#difficulty .btn")) {
+    $btn.addEventListener("click", () => {
+        game.difficulty = $btn.dataset.difficulty;
+        startGame();
+    });
+}
+
+get("#restart-btn").addEventListener("click", () => startGame(false));
+
+function get(selector) {
     return document.querySelector(selector);
 }
 
-function choose_symbol(e) {
-    $(".selected").classList.remove("selected");
-    e.target.classList.add("selected");
+function getAll(selector) {
+    return document.querySelectorAll(selector);
+}
 
-    player1.symbol = $(".selected span").className;
+function chooseSymbol(e) {
+    get(".symbol--selected").classList.remove("symbol--selected");
+    e.target.classList.add("symbol--selected");
+
+    player1.symbol = e.target.firstElementChild.className;
     player2.symbol = player1.symbol === O ? X : O;
 }
 
-function change_theme() {
-    game.dark_theme = !game.dark_theme;
-    let root = document.documentElement;
-
-    if (game.dark_theme)
-        root.classList.remove("light");
-    else
-        root.classList.add("light");
+function changeTheme() {
+    game.darkTheme = !game.darkTheme;
+    if (game.darkTheme) {
+        $sunSvg.style.display = "none";
+        $moonSvg.style.display = "block";
+        document.documentElement.classList.remove("light");
+    } else {
+        $sunSvg.style.display = "block";
+        $moonSvg.style.display = "none";
+        document.documentElement.classList.add("light");
+    }
 }
 
-function start_game(new_game = true) {
-    current_player = player1;
-    game.active_page = game_el;
-    board_el.classList.remove("game-draw");
+function startGame(reset = true) {
+    currentPlayer = player1;
+    $board.classList.remove("game-draw");
 
-    // reset scores if it's a new game
-    if (new_game) {
-        home_el.className = "inactive";
-        difficulty_el.className = "inactive";
-        game_el.className = "active";
+    if (reset) {
+        $home.className = "inactive";
+        $difficulty.className = "inactive";
+        $game.className = "active";
 
-        p1_name_el.innerText = player1.name;
-        p2_name_el.innerText = player2.name;
+        $gameMode.innerHTML = game.mode;
+        if (game.mode === SINGLE_PLAYER) {
+            $gameMode.innerHTML += `&nbsp; - &nbsp;${game.difficulty}`;
+        }
+
+        $p1Name.innerText = player1.name;
+        $p2Name.innerText = player2.name;
 
         player1.score = player2.score = 0;
 
-        $("#p1 .score-head span").className = player1.symbol;
-        $("#p2 .score-head span").className = player2.symbol;
-        update_scoreboard();
+        get("#p1 .score-head span").className = player1.symbol;
+        get("#p2 .score-head span").className = player2.symbol;
+        updateScoreboard();
     }
 
     // clear the board
     for (let i = 0; i < 9; ++i) {
-        board_state[i] = null;
-        cells_el[i].classList.remove("win");
+        boardState[i] = null;
+        $cells[i].classList.remove("win");
 
-        let symbol_el = cells_el[i].firstElementChild;
-        if (symbol_el)
-            cells_el[i].removeChild(symbol_el);
+        let $symbol = $cells[i].firstElementChild;
+        if ($symbol) {
+            $symbol.remove();
+        }
     }
 
-    highlight_current_player();
-    enable_cell_clicks();
+    highlightCurrentPlayer();
+    enableCellClicks();
 }
 
-function update_scoreboard() {
-    $("#p1 .score").innerText = player1.score;
-    $("#p2 .score").innerText = player2.score;
+function updateScoreboard() {
+    get("#p1 .score").innerText = player1.score;
+    get("#p2 .score").innerText = player2.score;
 }
 
-function highlight_current_player() {
-    $(".score-info.turn").classList.remove("turn");
+function highlightCurrentPlayer() {
+    get(".score-info.turn").classList.remove("turn");
 
-    if (current_player === player1)
-        $("#p1").classList.add("turn");
-    else
-        $("#p2").classList.add("turn");
+    if (currentPlayer === player1) {
+        get("#p1").classList.add("turn");
+    } else {
+        get("#p2").classList.add("turn");
+    }
 }
 
-function draw_symbol(player) {
+function drawSymbol(player) {
     const symbol = document.createElement("span");
     symbol.className = player.symbol;
     return symbol;
 }
 
-function enable_cell_clicks() {
-    get_empty_cells().forEach(i => {
-        cells_el[i].addEventListener("click", game.play_sound);
-        cells_el[i].onclick = e => make_move(e);
+function enableCellClicks() {
+    getEmptycells().forEach((i) => {
+        $cells[i].addEventListener("click", makeMove);
     });
 }
 
-function disable_cell_clicks() {
-    cells_el.forEach(cell => {
-        cell.removeEventListener("click", game.play_sound);
-        cell.onclick = null;
+function disableCellClicks() {
+    $cells.forEach(($cell) => {
+        $cell.removeEventListener("click", makeMove);
     });
 }
 
-function get_empty_cells(given_state = board_state) {
-    return given_state.map((_, i) => i)
-        .filter(i => given_state[i] === null);
+function getEmptycells(state = boardState) {
+    return state.map((_, i) => i).filter((i) => state[i] === null);
 }
 
-function make_move(e) {
-    game.play_sound();
+function makeMove(e) {
+    const $cell = e.target;
+    $cell.append(drawSymbol(currentPlayer));
+    $cell.removeEventListener("click", makeMove);
 
-    const cell = e.target;
-    cell.appendChild(draw_symbol(current_player));
-    cell.onclick = null;
-    cell.removeEventListener("click", game.play_sound);
+    const cellIndex = Number($cell.dataset.index);
+    boardState[cellIndex] = currentPlayer.symbol;
 
-    const cell_index = Number(cell.getAttribute("data-index"));
-    board_state[cell_index] = current_player.symbol;
-
-    const result = get_game_result(board_state);
+    const result = getGameResult(boardState);
     if (result === null) {
-        current_player = current_player === player1 ? player2 : player1;
-        highlight_current_player();
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+        highlightCurrentPlayer();
 
-        if (game.mode === "single-player" && current_player === player2)
-            computer_move();
+        if (game.mode === SINGLE_PLAYER && currentPlayer === player2) {
+            computerMove();
+        }
     } else {
-        game_over(result);
+        gameOver(result);
     }
 }
 
-function get_game_result(board_state) {
-    if (check_board(player1, board_state))
+function getGameResult(boardState) {
+    if (checkBoard(player1, boardState)) {
         return player1;
-    else if (check_board(player2, board_state))
+    } else if (checkBoard(player2, boardState)) {
         return player2;
-    else
-        return get_empty_cells(board_state).length === 0 ? DRAW : null;
-}
-
-function check_board(player) {
-    for (let combo of WIN_COMBOS) {
-        if (combo.every(i => board_state[i] === player.symbol))
-            return combo;
+    } else {
+        return getEmptycells(boardState).length === 0 ? DRAW : null;
     }
 }
 
-function game_over(player) {
-    disable_cell_clicks();
+function checkBoard(player) {
+    for (let combo of WIN_COMBOS) {
+        if (combo.every((i) => boardState[i] === player.symbol)) {
+            return combo;
+        }
+    }
+}
+
+function gameOver(player) {
+    disableCellClicks();
 
     if (player === DRAW) {
-        board_el.classList.add("game-draw");
+        $board.classList.add("game-draw");
     } else {
         player.score++;
-        check_board(player, board_state).forEach(i => cells_el[i].classList.add("win"));
+        checkBoard(player, boardState).forEach((i) => $cells[i].classList.add("win"));
     }
 
-    update_scoreboard();
+    updateScoreboard();
 }
 
-function computer_move() {
-    disable_cell_clicks();
+function computerMove() {
+    disableCellClicks();
 
-    const next_move = choose_computer_move();
+    const move = chooseComputerMove();
     setTimeout(() => {
-        enable_cell_clicks();
-        cells_el[next_move].click();
+        enableCellClicks();
+        $cells[move].click();
     }, 500);
 }
 
-function choose_computer_move() {
-    const empty_cells = get_empty_cells();
+function chooseComputerMove() {
+    const emptyCells = getEmptycells();
 
-    if (game.difficulty === DIFFICULTY_EASY) {
+    if (game.difficulty === EASY) {
         // check if AI can win
-        for (let i of empty_cells) {
-            board_state[i] = current_player.symbol;
-            if (get_game_result(board_state) === player2) {
-                board_state[i] = null;
+        for (let i of emptyCells) {
+            boardState[i] = currentPlayer.symbol;
+            if (getGameResult(boardState) === player2) {
+                boardState[i] = null;
                 return i;
             }
 
-            board_state[i] = null;
+            boardState[i] = null;
         }
 
-        return get_random_move();
-    } else if (game.difficulty === DIFFICULTY_MEDIUM) {
+        return getRandomMove();
+    } else if (game.difficulty === MEDIUM) {
         // check if AI can win
-        for (let i of empty_cells) {
-            board_state[i] = current_player.symbol;
-            if (get_game_result(board_state) == player2) {
-                board_state[i] = null;
+        for (let i of emptyCells) {
+            boardState[i] = currentPlayer.symbol;
+            if (getGameResult(boardState) == player2) {
+                boardState[i] = null;
                 return i;
             }
 
-            board_state[i] = null;
+            boardState[i] = null;
         }
 
         // check if opponent can win
-        for (let i of empty_cells) {
-            board_state[i] = player1.symbol;
-            if (get_game_result(board_state) == player1) {
-                board_state[i] = null;
+        for (let i of emptyCells) {
+            boardState[i] = player1.symbol;
+            if (getGameResult(boardState) == player1) {
+                boardState[i] = null;
                 return i;
             }
 
-            board_state[i] = null;
+            boardState[i] = null;
         }
 
-        return Math.random() < 0.5 ? get_random_move() : get_best_move();
+        return Math.random() < 0.5 ? getRandomMove() : getBestMove();
     } else {
-        return get_best_move();
+        return getBestMove();
     }
 }
 
-function get_random_move() {
-    let empty_cells = get_empty_cells();
-    const random = Math.floor(Math.random() * empty_cells.length);
-    return empty_cells[random];
+function getRandomMove() {
+    let emptyCells = getEmptycells();
+    const random = Math.floor(Math.random() * emptyCells.length);
+    return emptyCells[random];
 }
 
-function get_best_move() {
-    let best_score = -Infinity,
-        best_move;
+function getBestMove() {
+    let bestScore = -Infinity,
+        bestMove;
 
-    const empty_cells = get_empty_cells();
-    for (let index of empty_cells) {
-        board_state[index] = player2.symbol;
+    const emptyCells = getEmptycells();
+    for (let index of emptyCells) {
+        boardState[index] = player2.symbol;
 
-        let score = minimax(board_state, false, 0, best_score, Infinity);
-        if (score > best_score) {
-            best_score = score;
-            best_move = index;
+        let score = minimax(boardState, false, 0, bestScore, Infinity);
+        if (score > bestScore) {
+            bestScore = score;
+            bestMove = index;
         }
 
-        board_state[index] = null;
+        boardState[index] = null;
     }
 
-    return best_move;
+    return bestMove;
 }
 
-function minimax(board_state, is_max, depth, alpha, beta) {
-    const result = get_game_result(board_state);
+function minimax(boardState, isMax, depth, alpha, beta) {
+    const result = getGameResult(boardState);
     if (result !== null) {
-        if (result === DRAW)
-            return 0;
+        if (result === DRAW) return 0;
 
         return result === player2 ? 100 - depth : -100 + depth;
     }
 
-    const empty_cells = get_empty_cells(board_state);
+    const emptyCells = getEmptycells(boardState);
 
-    if (is_max) {
-        let best_score = -Infinity;
-        for (let i of empty_cells) {
-            board_state[i] = player2.symbol;
-            best_score = Math.max(
-                best_score,
-                minimax(board_state, false, depth + 1, alpha, beta)
-            );
-            board_state[i] = null;
+    if (isMax) {
+        let bestScore = -Infinity;
+        for (let i of emptyCells) {
+            boardState[i] = player2.symbol;
+            bestScore = Math.max(bestScore, minimax(boardState, false, depth + 1, alpha, beta));
+            boardState[i] = null;
 
             /*
              * alpha is the score that the Max (current node) is considering.
@@ -382,23 +360,19 @@ function minimax(board_state, is_max, depth, alpha, beta) {
              * Now if Max finds a score that is greater than or equal to beta
              * (alpha >= beta), Min will stop exploring Max because whatever
              * move that Max would otherwise choose will be greater than or
-             * equal to alpha.  
+             * equal to alpha.
              */
-            alpha = Math.max(alpha, best_score);
-            if (alpha >= beta)
-                break;
+            alpha = Math.max(alpha, bestScore);
+            if (alpha >= beta) break;
         }
 
-        return best_score;
+        return bestScore;
     } else {
-        let best_score = Infinity;
-        for (let i of empty_cells) {
-            board_state[i] = player1.symbol;
-            best_score = Math.min(
-                best_score,
-                minimax(board_state, true, depth + 1, alpha, beta)
-            );
-            board_state[i] = null;
+        let bestScore = Infinity;
+        for (let i of emptyCells) {
+            boardState[i] = player1.symbol;
+            bestScore = Math.min(bestScore, minimax(boardState, true, depth + 1, alpha, beta));
+            boardState[i] = null;
 
             /*
              * alpha is the score that the Max (parent node) is considering.
@@ -408,11 +382,10 @@ function minimax(board_state, is_max, depth, alpha, beta) {
              * move that Min would otherwise choose will be less than or
              * equal to beta.
              */
-            beta = Math.min(beta, best_score);
-            if (beta <= alpha)
-                break;
+            beta = Math.min(beta, bestScore);
+            if (beta <= alpha) break;
         }
 
-        return best_score;
+        return bestScore;
     }
 }
